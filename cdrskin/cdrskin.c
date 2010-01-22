cbb9125bd9a7c6c379fdc3f51585c0638fd01324
Thomas Schmitt <scdbackup@gmx.net>
2010-01-22 15:23:15 +0000

    Lifted ban to derive GPLv3, extended copyright range to 2010

--- cdrskin/cdrskin.c
+++ cdrskin/cdrskin.c
@@ -3143,7 +3143,7 @@ set_severities:;
      int major, minor, micro;
 
      printf(
-"Cdrecord 2.01-Emulation Copyright (C) 2006-2009, see libburnia-project.org\n");
+"Cdrecord 2.01-Emulation Copyright (C) 2006-2010, see libburnia-project.org\n");
      if(o->fallback_program[0]) {
        char *hargv[2];
 
